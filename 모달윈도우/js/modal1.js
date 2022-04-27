bfc3086773467bc87b974aa256a07b429b43e5b4
dygreen <95523625+dygreen@users.noreply.github.com>
2022-04-27 21:21:38 +0900

    Add files via upload

---
+++ 모달윈도우/js/modal1.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	
+  // 각 목록을 클릭했을 때....
+ $(".menu").click(function(){
+  $(this).next().show(); //다음 형제인 .pop을 보이게함
+  $("html").css({overflowY:"hidden"}); //스크롤바 없앰
+  return false;
+ });
+
+  // close버튼과 검정배경영역을 클릭할때....
+ $(".close, .pop").click(function(){
+   $(".pop").hide(); //.pop을 안보이게 함
+   $("html").css({"overflow-y":"scroll"}); //html 스크롤 다시 보이게 함
+ });
+
+
+});
